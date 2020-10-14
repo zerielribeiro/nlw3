3745763b525048d56818cbcb4ac8d2e5a548be8d
zeriel <zerielr@gmail.com>
2020-10-14 19:29:48 -0300

    criando rotas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,16 @@
 import React from 'react';
-import {FiArrowRight} from 'react-icons/fi'
+
 
 import './styles/global.css';
-import './styles/pages/landing.css';
 
-import logoImg from './imagens/Logo.svg';
+import Routes from './routes';
+import { Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div id="page-landing">
-     <div className="content-wrapper">
-      <img src={logoImg}alt=""/>
-      <main>
-        <h1>Leve felicidade para o Mundo </h1>
-        <p>Visite Orfanato e mude o dia de muitas criaçãos</p>        
-       </main>
-       <div className="location">
-         <strong>Guarapuava</strong>
-         <span>Paraná</span>
-       </div>
-       <a href="" className='enter-app'>
-        <FiArrowRight size={26} color="rgba(0,0,0, 6.6)"/>
-       </a>
-     </div>
-    </div>
+    <Routes/>
+    
   );
 }
 
